dde1a6a6160d7786db943eb8dc50b6da7e94fc70
Victor Vega <victorvega1828@gmail.com>
2017-09-05 23:16:16 -0500

    json with unix timestamp

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var timestamp = require('normalize-date');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/:time', function(req, res) {
+  if(!isNaN(req.params.time)){
+    var data = {unix: req.params.time, natural: timestamp(+req.params.time)}
+  }
+  res.json(data);
 });
 
 module.exports = router;
