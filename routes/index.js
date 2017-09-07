995279928a3cc483491a06bbaa68e1184e178ada
Victor Vega <victorvega1828@gmail.com>
2017-09-06 22:54:54 -0500

    Configure for deployment

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var timestamp = require('normalize-date');
 var router = express.Router();
 
 /* GET home page. */
@@ -7,11 +6,35 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+function unixToNormal(date){
+  var unix = new Date(date * 1000);
+  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  var month = months[unix.getMonth()];
+  var day = unix.getDate();
+  var year = unix.getFullYear();
+  var result = month + ' ' + day + ', ' + year;
+
+  return result;
+}
+
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 router.get('/:time', function(req, res) {
   if(!isNaN(req.params.time)){
-    var data = {unix: req.params.time, natural: timestamp(+req.params.time)}
+    var data = {unix: +req.params.time, natural: unixToNormal(req.params.time)}
+    res.json(data);
+  }else{
+    var natural = new Date(req.params.time);
+    if(!isNaN(natural)){
+      var unix = natural / 1000;
+      var data = {unix: unix, natural: capitalize(req.params.time)}
+      res.json(data);
+    }else{
+      res.json({unix: null, natural: null});
+    }
   }
-  res.json(data);
 });
 
 module.exports = router;
